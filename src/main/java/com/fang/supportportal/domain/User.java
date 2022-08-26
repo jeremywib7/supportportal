a2abb839698820ed3b990103c9887384ff362b52
jeremy <jeremywib7@gmail.com>
2022-08-26 16:58:21 +0700

    configure user principal

---
+++ src/main/java/com/fang/supportportal/domain/User.java
@@ -0,0 +1,37 @@
+package com.fang.supportportal.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long Id;
+
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String email;
+    private String profileImageUrl;
+    private Date lastLoginDate;
+    private Date lastLoginDateDisplay;
+    private Date joinDate;
+    private String[] roles; //ROLE_USER{ read, edit }, ROLE_ADMIN{ delete }
+    private String[] authorities;
+    private boolean isActive;
+    private boolean isNotLocked;
+}
