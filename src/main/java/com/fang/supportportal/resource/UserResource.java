bdc5d6280c61d325c582baa37b20f4f20aff59e1
jeremy <jeremywib7@gmail.com>
2022-08-30 18:00:47 +0700

    add aplication marks

---
+++ src/main/java/com/fang/supportportal/resource/UserResource.java
@@ -0,0 +1,15 @@
+package com.fang.supportportal.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserResource {
+
+    @GetMapping("/home")
+    public String showUser() {
+        return "application marks";
+    }
+}
