cef676acdc13dce1a4673aee7e689665b3f4598c
jeremy <jeremywib7@gmail.com>
2022-08-31 16:53:46 +0700

    add security constant

---
+++ src/main/java/com/fang/supportportal/constant/SecurityConstant.java
@@ -0,0 +1,9 @@
+package com.fang.supportportal.constant;
+
+public class SecurityConstant {
+    public static final long EXPIRATION_TIME = 432_000_000; // 5 Days expressed in milliseconds
+    public static final String TOKEN_HEADER = "Bearer ";
+    public static final String JWT_TOKEN_HEADER = "Jwt-Token";
+    public static final String TOKEN_CANNOT_BE_VERIFIED = "Token Cannot be verified";
+    public static final String
+}
