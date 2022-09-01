168155178ce7b5e987f4e8a903fc653876456dd0
jeremy <jeremywib7@gmail.com>
2022-09-01 17:02:03 +0700

    configure JWTTokenProvider

--- src/main/java/com/fang/supportportal/constant/SecurityConstant.java
+++ src/main/java/com/fang/supportportal/constant/SecurityConstant.java
@@ -2,8 +2,15 @@ package com.fang.supportportal.constant;
 
 public class SecurityConstant {
     public static final long EXPIRATION_TIME = 432_000_000; // 5 Days expressed in milliseconds
-    public static final String TOKEN_HEADER = "Bearer ";
+    public static final String TOKEN_PREFIX = "Bearer ";
     public static final String JWT_TOKEN_HEADER = "Jwt-Token";
-    public static final String TOKEN_CANNOT_BE_VERIFIED = "Token Cannot be verified";
-    public static final String
+    public static final String TOKEN_CANNOT_BE_VERIFIED = "Token cannot be verified";
+    public static final String FANG_LLC = "Fang, LLC";
+    public static final String FANG_ADMINISTRATION = "User Management Portal";
+    public static final String AUTHORITIES = "Authorities";
+    public static final String FORBIDDEN_MESSAGE = "You need to log in to access this page";
+    public static final String ACCESS_DENIED_MESSAGE = "You do not have permission to access this page";
+    public static final String OPTIONS_HTTP_METHOD = "OPTIONS";
+    public static final String[] PUBLIC_URLS = {"/user/login", "/user/register", "/user/resetpassword/**",
+            "/user/image/**", ""} ;
 }
