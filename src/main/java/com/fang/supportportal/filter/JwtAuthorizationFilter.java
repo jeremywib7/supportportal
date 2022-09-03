1759b8bd53820c84ae85e2ba3beedac3c791fb67
jeremy <jeremywib7@gmail.com>
2022-09-03 16:59:54 +0700

    configure jwt auth filter

---
+++ src/main/java/com/fang/supportportal/filter/JwtAuthorizationFilter.java
@@ -0,0 +1,25 @@
+package com.fang.supportportal.filter;
+
+import com.fang.supportportal.constant.SecurityConstant;
+import com.fang.supportportal.utility.JWTTokenProvider;
+import lombok.AllArgsConstructor;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@AllArgsConstructor
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    private final JWTTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (request.getMethod().equalsIgnoreCase(SecurityConstant.OPTIONS_HTTP_METHOD)) {
+
+        }
+    }
+}
--- src/main/java/com/fang/supportportal/utility/JWTTokenProvider.java
+++ src/main/java/com/fang/supportportal/utility/JWTTokenProvider.java
@@ -10,16 +10,22 @@ import static java.util.Arrays.stream;
 
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.fang.supportportal.domain.UserPrincipal;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
+@Component
 public class JWTTokenProvider {
     @Value("${jwt.secret}")
     private String secret;
@@ -35,6 +41,29 @@ public class JWTTokenProvider {
     public List<GrantedAuthority> getAuthorities(String token) {
         String[] claims = getClaimsFromToken(token);
         return stream(claims).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    public Authentication getAuthentication(String username, List<GrantedAuthority> authorities,
+                                            HttpServletRequest request) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username,
+                null, authorities);
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        return authenticationToken;
+    }
+
+    public boolean isTokenValid(String username, String token) {
+        JWTVerifier verifier = getJWTVerifier();
+        return StringUtils.isNotEmpty(username) && !isTokenExpired(verifier, token);
+    }
+
+    public String getSubject(String token) {
+        JWTVerifier verifier= getJWTVerifier();
+        return verifier.verify(token).getSubject();
+    }
+
+    private boolean isTokenExpired(JWTVerifier verifier, String token) {
+        Date expiration = verifier.verify(token).getExpiresAt();
+        return expiration.before(new Date());
     }
 
     private String[] getClaimsFromToken(String token) {
